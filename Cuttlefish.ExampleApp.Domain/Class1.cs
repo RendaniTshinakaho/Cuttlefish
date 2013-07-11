17d64b0d89c88fbde044b5d75519ebe6f0d643c3
johannds <johann@aitako.com>
2013-07-11 20:29:50 +0200

    fixed up the domain generator, added a project for the example application domain

---
+++ Cuttlefish.ExampleApp.Domain/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuttlefish.ExampleApp.Domain
+{
+    public class Class1
+    {
+    }
+}
