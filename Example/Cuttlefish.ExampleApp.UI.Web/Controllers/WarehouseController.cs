f2069a4bc802e1478506b7d0aed7a912bb15c0e6
johannds <johann@aitako.com>
2013-07-17 20:05:27 +0200

    empty web project

---
+++ Example/Cuttlefish.ExampleApp.UI.Web/Controllers/WarehouseController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cuttlefish.ExampleApp.UI.Web.Controllers
+{
+    public class WarehouseController : Controller
+    {
+        //
+        // GET: /Warehouse/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
